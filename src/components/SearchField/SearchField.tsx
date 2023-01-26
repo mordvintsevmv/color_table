c5ca74f23d1eee7d370d2f15a71e5a959e0b6cec
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-01-27 02:48:22 +0400

    Styling Search Field

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -2,6 +2,8 @@ import './SearchField.scss'
 import {ChangeEvent, FC, useState} from "react";
 import {useTypedSelector} from "../../hooks/typedHooks";
 import {useActions} from "../../hooks/useActions";
+import {InputAdornment, Stack, TextField} from "@mui/material";
+import {Palette} from "@mui/icons-material";
 
 const SearchField: FC = () => {
 
@@ -10,19 +12,32 @@ const SearchField: FC = () => {
     const {page} = useTypedSelector(state => state.colorReducer)
 
     const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        if ((/\d/).test(event.target.value)){
+        if ((/\d/).test(event.target.value)) {
             setIdValue(event.target.value)
             findColorById(Number(event.target.value))
-        } else if (event.target.value === ''){
+        } else if (event.target.value === '') {
             setIdValue(event.target.value)
             fetchColors(page)
         }
     }
 
-    return(
-        <div>
-            <input value={idValue} placeholder={'search by id'} onChange={inputChangeHandler}/>
-        </div>
+    return (
+        <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            mt={3}
+            mb={6}
+        >
+            <TextField
+                InputProps={{
+                    startAdornment: <InputAdornment position="start"><Palette/></InputAdornment>,
+                }}
+                value={idValue}
+                placeholder={'search by id'}
+                size={'small'}
+                onChange={inputChangeHandler}/>
+        </Stack>
     )
 }
 
