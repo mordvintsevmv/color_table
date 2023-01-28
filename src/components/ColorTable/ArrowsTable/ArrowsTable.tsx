9af6154428d2b986aa4b1cf2a27f403f0f224e58
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-01-28 21:21:57 +0400

    Implementing Page number into URL

--- src/components/ColorTable/ArrowsTable/ArrowsTable.tsx
+++ src/components/ColorTable/ArrowsTable/ArrowsTable.tsx
@@ -1,21 +1,36 @@
-import React, {FC} from "react";
+import React, {FC, useEffect} from "react";
 import {Button, Stack} from "@mui/material";
 import {ArrowBackIosNew, ArrowForwardIos} from "@mui/icons-material";
 import {useTypedSelector} from "../../../hooks/typedHooks";
 import {useActions} from "../../../hooks/useActions";
+import {useSearchParams} from "react-router-dom";
 
 
 const ArrowsTable: FC = () => {
 
     const {page, total_pages, total_colors , per_page} = useTypedSelector(state => state.colorReducer)
     const {setCurrentPage} = useActions()
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    useEffect(()=>{
+        const searchPage = searchParams.get('page')
+        if (searchPage){
+            setCurrentPage(Number(searchPage))
+        }
+    },[])
 
     const prevPageHandler = () => {
         setCurrentPage(page - 1)
+
+        searchParams.set('page', String(page - 1))
+        setSearchParams(searchParams)
     }
 
     const nextPageHandler = () => {
         setCurrentPage(page + 1)
+
+        searchParams.set('page', String(page + 1))
+        setSearchParams(searchParams)
     }
 
     return(
