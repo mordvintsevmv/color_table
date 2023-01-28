737c488cb59c88326163563e82c2ac1d3a0142b6
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-01-28 21:12:47 +0400

    Implementing Search ID into URL

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,8 +1,9 @@
-import {ChangeEvent, FC, useState} from "react";
+import {ChangeEvent, FC, useEffect, useState} from "react";
 import {useTypedSelector} from "../../hooks/typedHooks";
 import {useActions} from "../../hooks/useActions";
 import {InputAdornment, Stack, TextField} from "@mui/material";
 import {Palette} from "@mui/icons-material";
+import {useSearchParams} from "react-router-dom";
 
 const SearchField: FC = () => {
 
@@ -10,13 +11,35 @@ const SearchField: FC = () => {
     const {findColorById, fetchColors} = useActions()
     const {page} = useTypedSelector(state => state.colorReducer)
 
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    useEffect(()=>{
+        const searchID = searchParams.get('searchID')
+        if(searchID){
+            setIdValue(searchID)
+        }
+    },[])
+
+    useEffect(()=>{
+        if (idValue !== ''){
+            findColorById(Number(idValue))
+        }
+    },[idValue])
+
     const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         if ((/\d/).test(event.target.value)) {
+
             setIdValue(event.target.value)
-            findColorById(Number(event.target.value))
+
+            searchParams.set('searchID', event.target.value)
+            setSearchParams(searchParams)
         } else if (event.target.value === '') {
+
             setIdValue(event.target.value)
             fetchColors(page)
+
+            searchParams.delete('searchID')
+            setSearchParams(searchParams)
         }
     }
 
